0cf02766a1d4146acba5b5cc6198a29b3b6158e2
Anjalithakur44 <67846829+Anjalithakur44@users.noreply.github.com>
2024-03-20 23:06:31 +0530

    Add files via upload

---
+++ array1d1.java
@@ -0,0 +1,14 @@
+//print all even elements using for each loop
+public class array1d1 {
+    public static void main(String[] args) {
+        int arr[]={34,21,54,65,43};
+        for(int i:arr)
+        {
+            if(i%2==0)
+            {
+                System.out.print(i+" ");
+            }
+        }
+    }
+    
+}
