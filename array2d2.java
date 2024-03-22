d0549ecdcb44926e2ceba1a774a3ab9b5d4e1558
Anjali Thakur <67846829+Anjalithakur44@users.noreply.github.com>
2024-03-22 17:37:54 +0530

    Add files via upload

---
+++ array2d2.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+public class array2d2 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a[][]=new int[100][100];
+        int i,j,m,n;
+        System.out.println("Size of array=");
+        m=sc.nextInt();
+        n=sc.nextInt();
+        System.out.println("Enter array elements=");
+        for(i=0;i<m;i++)
+        {
+            for(j=0;j<n;j++)
+            {
+                a[i][j]=sc.nextInt();
+            }
+        }
+        //logic for printing elements above secondary diagonal for square matrix
+        for(i=0;i<m;i++)
+        {
+            for(j=0;j<n;j++)
+            {
+               if(i+j<(n-1))
+               {
+                System.out.print(a[i][j]+" ");
+               }
+              
+            }
+        }
+    }
+}
